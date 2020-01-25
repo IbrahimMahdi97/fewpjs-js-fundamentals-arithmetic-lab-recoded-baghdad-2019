1c0f6e1531466606e6b15cd5d4638761648021c7
Ibrahim Mahdi <ibrahimmahdi115@gmail.com>
2020-01-25 13:58:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 // Write your code here
-const val = 31;
-const val1 = 2;
-const multiply = (val * val1);
+const num1 = 31;
+const num2 = 2;
+const multiply = (num1 * num2);
 const random = Math.floor( Math.random() * 1000 ) + 1
 const mod = 16 % 6
 const max = Math.max( 5, 10, 15, 20 )
